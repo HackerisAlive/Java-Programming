5b042e186ba6099e92954d32d85c953174e4fbc0
HackerisAlive <98787245+HackerisAlive@users.noreply.github.com>
2022-03-30 06:15:29 +0530

    Add files via upload

---
+++ 1. First Program/2. Myth about the file name and class name/Main.java
@@ -0,0 +1,20 @@
+/***** File name: Main.java ******/
+
+// To Avoid the error first of all don't write public class classname
+// And then compile this code by file name javac Main.java
+// Then we will get 2 .class file
+// To execute that class file write java Ayush or java Ketan
+
+class Ayush { // class name is Ayush
+	public static void main(String[] args)
+	{
+		System.out.println("Hello I am Ayush");
+	}
+}
+
+class Ketan { // class name is Ketan
+	public static void main(String[] args)
+	{
+		System.out.println("Hello I am Ketan");
+	}
+}
